044146b31acee84f6327388ce86f84e7262d2c43
HoutarouM <marian.horoshko@gmail.com>
2022-12-13 08:04:02 +0100

    add data binding

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,22 +9,16 @@ export class FormComponent implements OnInit {
 
   constructor() { }
 
-  imie: string = "";
-
-  email:string = "Prosze podac email";
-
-  produkt:string = "";
-
-  ilosc: number = 0;
-
-  wiadomosc: string = "jesli masz uwage wpisz je tutaj";
+  nameandlast: string = "";
+  email: string = "";
+  product: string = "";
+  count: number = 0;
+  attention: string = "";
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    console.log("imie: " + this.imie + "email: " + this.email + "produkt: " + this.produkt+ "ilosc: " + this.ilosc + "wiadomosc: " + this.wiadomosc);
+    console.log("imie: " + this.nameandlast + "email: " + this.email + "produkt: " + this.product + " ilosc: " + this.count + " wiadomosc: " + this.attention);
   }
-
-  
 }
