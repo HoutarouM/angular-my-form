a41227561ac1d80f1350d22938dd468e2f24cf1f
HoutarouM <marian.horoshko@gmail.com>
2022-12-13 07:31:24 +0100

    zamowienie form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,7 +9,22 @@ export class FormComponent implements OnInit {
 
   constructor() { }
 
+  imie: string = "";
+
+  email:string = "Prosze podac email";
+
+  produkt:string = "";
+
+  ilosc: number = 0;
+
+  wiadomosc: string = "jesli masz uwage wpisz je tutaj";
+
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    console.log("imie: " + this.imie + "email: " + this.email + "produkt: " + this.produkt+ "ilosc: " + this.ilosc + "wiadomosc: " + this.wiadomosc);
+  }
+
+  
 }
